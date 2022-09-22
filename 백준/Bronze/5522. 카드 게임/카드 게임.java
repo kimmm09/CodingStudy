6e87eb7dd6c9c65a0ad07fdc811dbbb2e32af244
kimmm09 <112105596+kimmm09@users.noreply.github.com>
2022-09-22 23:24:15 +0900

    [Bronze V] Title: 카드 게임, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D, E;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		E = sc.nextInt();
+		
+		System.out.println(A+B+C+D+E);
+				
+		
+		sc.close();
+	}	
+}
