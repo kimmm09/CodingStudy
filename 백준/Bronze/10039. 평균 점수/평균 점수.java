ba7392f9dd1236e16a40230a2bd43b0d207313b8
kimmm09 <112105596+kimmm09@users.noreply.github.com>
2022-10-26 09:21:09 +0900

    [Bronze IV] Title: 평균 점수, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, d, e;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		e = sc.nextInt();
+		
+		if(a<40) {
+			a = 40;
+		}
+		if(b<40) {
+			b = 40;
+		}
+		if(c<40) {
+			c = 40;
+		}
+		if(d<40) {
+			d = 40;
+		}
+		if(e<40) {
+			e = 40;
+		}
+		System.out.println((a+b+c+d+e)/5);
+		
+		sc.close();
+	}
+}
