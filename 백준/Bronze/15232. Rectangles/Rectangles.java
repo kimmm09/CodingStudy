48f95c9a1e14ea716cfe19a59882dec121aca238
kimmm09 <112105596+kimmm09@users.noreply.github.com>
2022-11-22 14:35:28 +0900

    [Bronze IV] Title: Rectangles, Time: 224 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int R, C;
+		R = sc.nextInt();
+		C = sc.nextInt();
+		
+		for(int i=0; i<R; i++) {
+			for(int j=0; j<C; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		sc.close();
+	}
+}
